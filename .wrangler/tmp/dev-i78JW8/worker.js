3f5d7d7c1b83afe31e03c022b15a11d8d386db0a
Shivarjun <shivarjun.bhattarai@gmail.com>
2026-04-25 22:42:07 -0500

    remove secrets from wrangler.jsonc and add them to secrets.toml

--- .wrangler/tmp/dev-i78JW8/worker.js
+++ .wrangler/tmp/dev-i78JW8/worker.js
@@ -3812,7 +3812,7 @@ var jsonError = /* @__PURE__ */ __name(async (request, env, _ctx, middlewareCtx)
 }, "jsonError");
 var middleware_miniflare3_json_error_default = jsonError;
 
-// .wrangler/tmp/bundle-3s2n0D/middleware-insertion-facade.js
+// .wrangler/tmp/bundle-E94OvZ/middleware-insertion-facade.js
 var __INTERNAL_WRANGLER_MIDDLEWARE__ = [
   middleware_ensure_req_body_drained_default,
   middleware_miniflare3_json_error_default
@@ -3844,7 +3844,7 @@ function __facade_invoke__(request, env, ctx, dispatch, finalMiddleware) {
 }
 __name(__facade_invoke__, "__facade_invoke__");
 
-// .wrangler/tmp/bundle-3s2n0D/middleware-loader.entry.ts
+// .wrangler/tmp/bundle-E94OvZ/middleware-loader.entry.ts
 var __Facade_ScheduledController__ = class ___Facade_ScheduledController__ {
   constructor(scheduledTime, cron, noRetry) {
     this.scheduledTime = scheduledTime;
